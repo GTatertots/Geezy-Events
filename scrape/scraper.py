a7191670757e37874b9b15755bd70ec92bad85e0
Keaton Brinkerhoff <keatonbrink@gmail.com>
2023-11-17 10:53:29 -0700

    Further with scraper

--- scrape/scraper.py
+++ scrape/scraper.py
@@ -4,21 +4,65 @@ import time
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+TEST = False
 
-title = driver.title
+def main():
+    if TEST:
+        init_test()
+        return
+    StGeorgeWebsite()
 
-driver.implicitly_wait(0.5)
+def StGeorgeWebsite():
+    driver.get("https://www.sgcity.org/eventcalendar/")
+    week_button_container = driver.find_elements(by=By.CLASS_NAME, value="fc-button-group")
+    for possible_container in week_button_container:
+        if possible_container.text == "MONTH\nWEEK\nDAY":
+            week_button = possible_container
+    week_button = week_button.find_element(by=By.CLASS_NAME, value="fc-listWeek-button")
+    week_button.click()
+    fc_list_table = driver.find_elements(by=By.CLASS_NAME, value="fc-list-table")[1]
+    fc_list_table = fc_list_table.find_element(by=By.TAG_NAME, value="tbody")
+    event_containers = fc_list_table.find_elements(by=By.CLASS_NAME, value="fc-list-item")
+    for event in event_containers:
+        time_element = event.find_element(by=By.CLASS_NAME, value="fc-list-item-time")
+        time_text = time_element.text
+        event_start_time = time_text.split("-")[0].strip()
+        event_end_time = time_text.split("-")[1].strip()
+        clickable_title = event.find_element(by=By.CLASS_NAME, value="fc-list-item-title").find_element(by=By.TAG_NAME, value="a")
+        event_title = clickable_title.text
+        clickable_title.click() 
+        model_body = driver.find_elements(by=By.CLASS_NAME, value="modal-body")
+        print(len(model_body))
+        for elm in model_body:
+            temp = elm.find_elements(by.CLASS_NAME, value="viewLocation")
+            print(len(temp))
+            print("\n\n\n")
+            print(elm.tag_name)
+            print("\nTREE\n\n")
+        # time.sleep(10)
+        return
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    # time.sleep(10)    
 
-text_box.send_keys("Selenium")
-submit_button.click()
+def init_test():
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
+    title = driver.title
 
-# time.sleep(10)
+    driver.implicitly_wait(0.5)
+
+    text_box = driver.find_element(by=By.NAME, value="my-text")
+    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+    text_box.send_keys("Selenium")
+    submit_button.click()
+
+    message = driver.find_element(by=By.ID, value="message")
+    text = message.text
+
+    time.sleep(10)
+
+if __name__ == "__main__":
+    main()
 
 driver.quit()
