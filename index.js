a9d89e1f633630bcee1b2e618f042d7f677495c8
Keaton Brinkerhoff <keatonbrink@gmail.com>
2023-11-17 15:53:42 -0700

    Add ability to grab from model

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ var app = express();
 const port = 8080;
 
 app.get('/events', function (req, res) {
-
+  model.getEvents().then((events) => {
+    res.status(200).json(events);
+  }).catch((err) => {
+    res.status(500).send("Error: " + err);
+  });
 });
 
 app.get('/events/:eventID', function (req, res) {
-
+  model.getSingleEvent(req.params.eventID).then((event) => {
+    res.status(200).json(event);
+  }).catch((err) => {
+    res.status(500).send("Error: " + err);
+  });
 });
 
 app.put('/events/:eventID', function (req, res) {
@@ -35,5 +43,5 @@ app.post('/events', function (req, res) {
 
 
 app.listen(8080, function () {
-        console.log('Server running on port 8080...');
+  console.log('Server running on port 8080...');
 });
